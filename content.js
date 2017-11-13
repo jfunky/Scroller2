e69ba0e018065d1b13a496db9440266fa9771ca0
jfunky <jasminea.soltani@gmail.com>
2017-11-12 23:06:17 -0500

    fixed to work on all sites

--- content.js
+++ content.js
@@ -1,29 +1,20 @@
 // content.js
+function pageScroll() {
+    //works on some sites but not FB, Twitter, basicanalogcircuits
+    window.scrollBy(0,10);
+
+    scrolldelay = setTimeout(pageScroll,10);
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      pageScroll();
 
-      console.log(firstHref);
-
-      function pageScroll() {
-          //works on some sites but not FB, Twitter, basicanalogcircuits
-          window.scrollBy(0,10);
-
-          // works on FB and some other sites
-          // window.document.documentElement.scrollBy(0,10);
-          
-          scrolldelay = setTimeout(pageScroll,10);
+      if($(window).scrollTop() + $(window).height() == $(document).height()) {
+         alert("bottom!");
+         console.log(window.scrollY);
       }
-
-      window.addEventListener('load', pageScroll);
-
-      $(window).scroll(function() {
-         if($(window).scrollTop() + $(window).height() == $(document).height()) {
-             alert("bottom!");
-             console.log(window.scrollY);
-         }
-      });
     }
   }
 );
